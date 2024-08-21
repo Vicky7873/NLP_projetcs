1f3448313a075b6876c0a160ef93700db69c684e
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-08-21 09:31:47 +0530

    data_transformation modular Coding added

---
+++ src/textSummarizer/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,15 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.conponents.data_transformation import DataTransformation
+from textSummarizer.logging import logger
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        config = ConfigurationManager().get_data_transformation_config()
+        self.data_transformation = DataTransformation(config)
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
